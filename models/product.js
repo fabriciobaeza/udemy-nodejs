dcaab746a793018343703531771abeb58ecc2a60
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-02-07 20:54:01 -0500

    Adding the Product-Delete Functionality

--- models/product.js
+++ models/product.js
@@ -42,7 +42,17 @@ module.exports = class Product {
         console.log(err);
         });
       }
-      
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err) {
+          
+        }
+      })
     });
   }
 
@@ -54,6 +64,6 @@ module.exports = class Product {
     getProductsFromFile(products => {
       const product = products.find(p => p.id === id);
       cb(product);
-    })
+    });
   }
 };
