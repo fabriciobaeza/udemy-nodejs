df059859e5e3a029d3ba2ec92b010d5d60db1de0
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-02-21 14:59:45 -0500

    Storing Multiple Products in the Cart

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('60314b23879ed84630134590')
     .then(user => {
-        req.user =user;
+        req.user = new User(user.name, user.email, user.cart, user._id);
         next();
     })
     .catch(err => console.log(err));
