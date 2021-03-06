97eaee54dcd50ed36d19284abb8a5367ed7b0f5b
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-03-06 12:25:38 -0500

    Using the Session Middleware

--- app.js
+++ app.js
@@ -1,55 +1,62 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my secret", resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
-  User.findById('603ab101892fab46702cb39a')
-    .then(user => {
+  User.findById("603ab101892fab46702cb39a")
+    .then((user) => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://fabriciobaeza:projectmayhem75@cluster0.n4hv1.mongodb.net/shop?retryWrites=true&w=majority')
-.then(result => {
-  User.findOne().then(user => {
-      if(!user) {
+mongoose
+  .connect(
+    "mongodb+srv://fabriciobaeza:projectmayhem75@cluster0.n4hv1.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
         const user = new User({
-          name: 'Edgehead',
-          email: 'test@test.com',
+          name: "Edgehead",
+          email: "test@test.com",
           cart: {
-            items: []
-          }
+            items: [],
+          },
         });
         user.save();
       }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-  app.listen(3000);
-})
-.catch(err => {
-  console.log(err);
-});
