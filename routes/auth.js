433b973ba12e3fdfdc94806ba7958fb662758cc8
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-03-07 16:40:59 -0500

    Implementing an Authentication

--- routes/auth.js
+++ routes/auth.js
@@ -6,9 +6,11 @@ const router = express.Router();
 
 router.get("/login",authController.getLogin);
 
+router.get('/signup', authController.getSignup);
+
 router.post('/login', authController.postLogin);
 
-router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
