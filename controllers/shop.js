6a882c3d9b56f98b4018f2c1e13c88cfb2191823
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-03-28 14:45:10 -0400

    Setting File Type Headers

--- controllers/shop.js
+++ controllers/shop.js
@@ -156,8 +156,13 @@ exports.getInvoice = (req, res, next) => {
   const invoicePath = path.join("data", "invoices", invoiceName);
   fs.readFile(invoicePath, (err, data) => {
     if (err) {
-     return next(err);
+      return next(err);
     }
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'inline; filename="' + invoiceName + '"'
+    );
     res.send(data);
   });
 };
